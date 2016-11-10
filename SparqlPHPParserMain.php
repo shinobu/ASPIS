2f4f6f00885cd283b628f2f3a0fd88a79da999ba
Natanael Arndt <arndtn@gmail.com>
2016-11-10 12:29:56 +0100

    Reorganize Code of SparqlPHPParserMain.php

--- SparqlPHPParserMain.php
+++ SparqlPHPParserMain.php
@@ -3,42 +3,50 @@
 include 'lib/sparql.lex.php';
 include 'lib/sparql.php';
 
-class SparqlPHPParserMain {
-	//root gets set in the parser
-	public $root = null;
-  public $parser = null;
+class SparqlPHPParserMain
+{
+    //root gets set in the parser
+    public $root = null;
+    public $parser = null;
 
-  private function parse($fp) {
-		if (!isset($parser)) {
-			$this->parser = new SparqlPHPParser($this);
-		}
-    $scanner = new SparqlLexer($fp);
-	    while ($x = $scanner->nextToken())
-		  {
-		    if($x->type != -1) {
-					$this->parser->doParse($x->type, $x);
-				} else {
-					$err = 'Invalid Input in line ' . $x->line . '. Problem with: ' . $x->value . fgets($fp, 15);
-					throw new Exception($err);
-				}
-		  }
-			$this->parser->doParse(0);
-			
-  }
+    private function parse($filePointer)
+    {
+        if (!isset($this->parser)) {
+            $this->parser = new SparqlPHPParser($this);
+        }
 
-	function parseString($string) {
-		$fp = fopen("php://memory", 'r+');
-		fwrite($fp, $string);
-		rewind($fp);
-		return $this->parse($fp);
-	}
+        $scanner = new SparqlLexer($filePointer);
+        while ($token = $scanner->nextToken()) {
+            if ($token->type != -1) {
+                $this->parser->doParse($token->type, $token);
+            } else {
+                $err = 'Invalid Input in line ' . $token->line . '. Problem with: ' . $token->value . fgets($filePointer, 15);
+                throw new Exception($err);
+            }
+        }
+        $this->parser->doParse(0);
+    }
 
-	function parseFile($file) {
-    $fp = fopen($file, 'r+') or die('Unable to open file with path: ' . $file);
-		return $this->parse($fp);
-	}
-	
-	function resetParser() {
-		//TODO
-	}
+    public function parseString($string)
+    {
+        $filePointer = fopen("php://memory", 'r+');
+        fwrite($filePointer, $string);
+        rewind($filePointer);
+        return $this->parse($filePointer);
+    }
+
+    public function parseFile($file)
+    {
+        try {
+            $filePointer = fopen($file, 'r+');
+        } catch (Exception $e) {
+            throw new Exception('Unable to open file with path: ' . $file, 0, $e);
+        }
+        return $this->parse($filePointer);
+    }
+
+    public function resetParser()
+    {
+        //TODO
+    }
 }
