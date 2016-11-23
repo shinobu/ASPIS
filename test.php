c9780d7943c903b68db15b5642a88ea71a8cc281
shinobu <niehoff.fabian@web.de>
2016-11-23 17:40:14 +0100

    revert wrong regex change from 5d5e0ae

--- test.php
+++ test.php
@@ -3,7 +3,8 @@
 include 'SparqlPHPParserMain.php';
 
 $parser = new SparqlPHPParserMain();
-$parser->parseString('@Base <http://guessit.com> Prefix abc:<https://www.getit> Select * where { <abc> ?b ?c }');
+$parser->parseString('@Base <http://guessit.com> 
+Prefix abc:<https://www.getit> Select * where { <abc> ?b ?c }');
 print_r(PHP_EOL);
 print_r($parser->root->query);
  ?>
